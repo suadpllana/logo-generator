6da0b1c67957bb33b641d71de3bc3e0150527a50
oltapllana <oltapllana@gmail.com>
2024-10-14 02:32:44 +0200

    first commit

--- src/Logo.jsx
+++ src/Logo.jsx
@@ -1,34 +1,57 @@
-import React from 'react'
-import {useRef} from "react"
-import {useState} from "react"
-const Logo = () => {
-    const inputRef = useRef(null)
-    const [imageData , setImageData] = useState(false)
-    const [companyName , setCompanyName ] = useState("")
-    const API_KEY = import.meta.env.VITE_API_KEY
+import React, { useRef, useState } from "react";
 
-        function getImage(){
-          const company = inputRef.current.value.replace(/\s+/g, '');
-          
-        const companyName = `https://img.logo.dev/${company}.com?token=${API_KEY}`
-        setCompanyName(companyName)
-        inputRef.current.value =``
-        }
-        function enterFunction(e){
-            if(e.key === "Enter"){
-                getImage()
-            }
-        }
-    
+const Logo = () => {
+  const inputRef = useRef(null);
+  const [companyName, setCompanyName] = useState("");
+  const [isDownloaded , setIsDownloaded] = useState(false)
+  const API_KEY = import.meta.env.VITE_API_KEY;
+
+  function getImage() {
+    const company = inputRef.current.value.replace(/\s+/g, "");
+    const companyLogoUrl = `https://img.logo.dev/${company}.png?token=${API_KEY}`;
+    setCompanyName(companyLogoUrl);
+    inputRef.current.value = ``;
+    setIsDownloaded(false)
+  }
+
+  function enterFunction(e) {
+    if (e.key === "Enter") {
+      getImage();
+    }
+  }
+  const download = () => {
+    setIsDownloaded(true)
+  }
+
   return (
     <div>
-      <h1>Generate logo by writing any big companies name</h1>
-      <input onKeyDown={(e) => enterFunction(e)} type="text" placeholder="Write the name here ex. apple" ref={inputRef} />
-      <button onClick={getImage}>Search</button><br />
-      {companyName ? <img src={companyName} alt="company not found" /> : <></>}
+      <h1>Generate logo by writing any big company's name</h1>
+      <input
+        onKeyDown={(e) => enterFunction(e)}
+        type="text"
+        placeholder="Write the name here ex. apple"
+        ref={inputRef}
+      />
+      <button onClick={getImage}>Search</button>
+      <br />
+    
+       {companyName && (
+ <div className="image-container">
+ <a target="_blank" href={companyName} download>
+
+ <img onClick={download} src={companyName} alt="Company logo" />
+ </a>
+
+
+<div className="tooltip">{isDownloaded ? "Downloaded" : "Download"}</div>
+</div>
+       )}
+         
+          
+      
       
     </div>
-  )
-}
+  );
+};
 
-export default Logo
+export default Logo;
